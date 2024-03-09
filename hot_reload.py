8ed7d4cad074558f8fbe5edeca70d351ecf4d613
anchal00 <anchal82199@gmail.com>
2024-03-09 22:58:27 +0530

    Decouple file diff code from watchdog code

--- hot_reload.py
+++ hot_reload.py
@@ -4,7 +4,7 @@ import sys
 import threading
 import time
 
-from utils import calculate_sha256_hash, get_py_files_in_dir
+from utils import get_py_files_in_dir, has_file_changed
 
 
 class HotReloadWatchDog(threading.Thread):
@@ -18,14 +18,12 @@ class HotReloadWatchDog(threading.Thread):
 
     def _spawn_process(self):
         if self._process is not None and self._process.poll() is None:
-            print("Reloading.....")
             self._process.kill()
             self._process.wait()
         self._process = subprocess.Popen(args=[sys.executable, self._module])
         print(f"Spawned new process with pid {self._process.pid}")
 
     def watch(self):
-        file_hash_dict = {}
         dir = os.getcwd()
         print(f"Hot Reload: Watching files under dir {dir}")
         while True:
@@ -33,13 +31,9 @@ class HotReloadWatchDog(threading.Thread):
             for file in files:
                 if file == "hot_reload.py":
                     continue
-                new_hash = calculate_sha256_hash(file)
-                old_hash = file_hash_dict.get(file)
-                if not old_hash:
-                    file_hash_dict[file] = new_hash
-                elif old_hash != new_hash:
-                    file_hash_dict[file] = new_hash
-                    print(f"File {file} changed !")
+                if has_file_changed(file):
+                    print(f"Changes detected in File {file}")
+                    print("Reloading.....")
                     self._spawn_process()
             time.sleep(1)
 
--- utils.py
+++ utils.py
@@ -3,14 +3,26 @@ import hashlib
 import os
 
 
-def calculate_sha256_hash(filename):
-    hash = hashlib.sha256()
+_FILE_HASHES = {}
+
+
+def _calculate_md5_hash(filename):
+    hash = hashlib.md5()
     bytarray = bytearray(128*1024)
     mv = memoryview(bytarray)
     with open(filename, 'rb', buffering=0) as f:
         while n := f.readinto(mv):
             hash.update(mv[:n])
     return hash.hexdigest()
+
+
+def has_file_changed(filename):
+    new_hash = _calculate_md5_hash(filename)
+    saved_hash = _FILE_HASHES.setdefault(filename, new_hash)
+    if saved_hash != new_hash:
+        _FILE_HASHES[filename] = new_hash
+        return True
+    return False
 
 
 def get_py_files_in_dir(dir):
